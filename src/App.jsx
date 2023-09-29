a20d83309857420994c9caa350fcdbac83ca3973
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-09-29 16:00:13 +0300

    added base setup of the dashboard

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Box bg="tomato" w="100%" p={4} color="white">
+      This is the Box
+    </Box>
+  );
+};
+
+export default App;
