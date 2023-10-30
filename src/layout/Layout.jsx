752d69274e462f99248d0fe45277052252bbb295
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-10-30 12:33:40 +0300

    added layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,37 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  useDisclosure,
+  useColorModeValue,
+  Drawer,
+  DrawerContent,
+} from "@chakra-ui/react";
 import { Sidebar, Header, Footer, Content } from "../components/index";
 
 const Layout = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box>
-      <Sidebar />
-      <Content />
-      <Header />
+    <Box bg={useColorModeValue("gray.100", "gray.900")}>
+      <Sidebar
+        onClose={() => onClose}
+        display={{ base: "none", md: "block" }}
+      />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full"
+      >
+        <DrawerContent>
+          <Sidebar onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      {/* mobilenav */}
+      <Header onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        <Content />
+      </Box>
       <Footer />
     </Box>
   );
