ef385e53193b6069e1fa8abb58b63094e8f487db
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-10-31 13:07:30 +0300

    added basic navigations

--- src/_nav.jsx
+++ src/_nav.jsx
@@ -6,6 +6,23 @@ const _nav = [
     icon: FiHome,
     to: "/dashboard",
   },
+  {
+    name: "test",
+    icon: FiHome,
+    to: "/test",
+    items: [
+      {
+        name: "Test 1",
+        to: "/test1",
+        icon: FiHome,
+      },
+      {
+        name: "Test 2",
+        to: "/test2",
+        icon: FiHome,
+      },
+    ],
+  },
 ];
 
 export default _nav;
