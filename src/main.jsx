90e6ce8e0dc9492fd0319dfdb7ac7570b73a6597
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-11-02 16:12:19 +0300

    setup primereact

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 
+// set up prime react
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "../node_modules/primeflex/primeflex.css";
+import "primeflex/themes/primeone-light.css";
+import "primeflex/themes/primeone-dark.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
