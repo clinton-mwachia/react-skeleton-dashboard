139f4af58e013f455644c1e3aebfc865d197e1a4
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-11-02 11:36:42 +0300

    added card component to the dashboard

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -1,5 +1,27 @@
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import { BsPerson } from "react-icons/bs";
+import { FiServer } from "react-icons/fi";
+import { GoLocation } from "react-icons/go";
+import Card from "./Card";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box maxW="7xl" mx={"auto"} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
+      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
+        <Card title={"Users"} stat={"5,000"} icon={<BsPerson size={"3em"} />} />
+        <Card
+          title={"Servers"}
+          stat={"1,000"}
+          icon={<FiServer size={"3em"} />}
+        />
+        <Card
+          title={"Datacenters"}
+          stat={"7"}
+          icon={<GoLocation size={"3em"} />}
+        />
+      </SimpleGrid>
+    </Box>
+  );
 };
 
 export default Dashboard;
