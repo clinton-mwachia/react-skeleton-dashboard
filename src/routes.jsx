73fe7e3d43f99cf7c73b968b2035dbf41e530fdd
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-12-09 17:45:45 +0300

    added routes for component and component2

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,14 +2,14 @@
 import { lazy } from "react";
 
 const Dashboard = lazy(() => import("./views/dashboard/Dashboard"));
-const Boxs = lazy(() => import("./views/box/Boxs"));
-const Test2 = lazy(() => import("./views/test2/Test2"));
+const Component = lazy(() => import("./views/component/Component"));
+const Component2 = lazy(() => import("./views/component2/Component2"));
 
 const routes = [
   { path: "/", exact: true, name: "Home" },
   { path: "/dashboard", name: "Dashboard", element: Dashboard },
-  { path: "/box", name: "Box", element: Boxs },
-  { path: "/test2", name: "Test2", element: Test2 },
+  { path: "/component", name: "Box", element: Component },
+  { path: "/component2", name: "Test2", element: Component2 },
 ];
 
 export default routes;
